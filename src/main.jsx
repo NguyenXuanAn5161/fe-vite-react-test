427a01055a1bbc971ff713128ce50863e03a730e
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-01-22 19:07:25 +0700

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { store } from "./redux/store";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
-      {/* <App /> */}
+      <App />
     </Provider>
   </React.StrictMode>
 );
