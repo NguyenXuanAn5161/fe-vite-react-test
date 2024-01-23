df7bfdeeb52d1581b0f90b03d34a40106088554e
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-01-23 16:16:53 +0700

    hoan thanh login and register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,87 +1,132 @@
-import { Button, Divider, Form, Input } from "antd";
+import { Button, Divider, Form, Input, message, notification } from "antd";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { callRegister } from "../../services/api";
+import "./register.scss";
 
 const RegisterPage = () => {
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const navigate = useNavigate();
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const onFinish = async (values) => {
+    const { fullName, email, password, phone } = values;
+    setIsSubmit(true);
+    const res = await callRegister(fullName, email, password, phone);
+    setIsSubmit(false);
+    if (res?.data?._id) {
+      message.success("Đăng ký tài khoản thành công!");
+      navigate("/login");
+    } else {
+      notification.error({
+        message: "Có lỗi xảy ra!",
+        description:
+          res.message && Array.isArray(res.message.length) > 0
+            ? res.message[0]
+            : res.message,
+        duration: 5,
+      });
+    }
   };
 
   return (
-    <div className="register-page" style={{ padding: "5%" }}>
-      <h3 style={{ textAlign: "center" }}>Đăng ký tài khoản</h3>
-      <Divider />
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
+    <div className="register-container">
+      <div
+        className="register-form"
         style={{
-          maxWidth: 600,
-          margin: "0 auto",
+          margin: "auto",
+          maxWidth: 500,
         }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        autoComplete="true"
       >
-        <Form.Item
-          label="Họ và tên"
-          name="fullname"
-          rules={[
-            {
-              required: true,
-              message: "Họ và tên không được để trống!",
-            },
-          ]}
+        <div className="register-header">
+          <h3 style={{ textAlign: "center" }}>Đăng ký tài khoản</h3>
+        </div>
+        <Divider />
+        <Form
+          name="basic"
+          style={{
+            maxWidth: 450,
+            margin: "0 auto",
+          }}
+          onFinish={onFinish}
+          autoComplete="true"
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Email không được để trống!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Mật khẩu"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Mật khẩu không được để trống!",
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          label="Số điện thoại"
-          name="phone"
-          rules={[
-            {
-              required: true,
-              message: "Số điện thoại không được để trống!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            label="Họ và tên"
+            name="fullName"
+            rules={[
+              {
+                required: true,
+                message: "Họ và tên không được để trống!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Email không được để trống!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            label="Mật khẩu"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Mật khẩu không được để trống!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            label="Số điện thoại"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                message: "Số điện thoại không được để trống!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Đăng ký
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+          // wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Button
+              type="primary"
+              htmlType="submit"
+              loading={isSubmit}
+              style={{ width: "100%" }}
+            >
+              Đăng ký
+            </Button>
+          </Form.Item>
+        </Form>
+        <Divider>Hoặc</Divider>
+        <p>
+          Đã có tài khoản?
+          <span className="text-normal">
+            <Link style={{ textDecoration: "none " }} to="/login">
+              {" "}
+              Đăng nhập
+            </Link>
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
